c059324f782e1dd57dafb50db4bf4924647fd0ca
hrjchem <55139703+hrjchem@users.noreply.github.com>
2019-11-11 23:31:50 +0800

    Update planets.py

--- planets.py
+++ planets.py
@@ -9,10 +9,11 @@ def orbit():
         t[i].up()
         t[i].goto(0,40*i+40)
         t[i].down()
-        t[i].shape('circle')
-        t[i].color(colors[i])
     for j in range(360):
         while True:
             x=j*2*math.pi/360
-            t[i].goto((40*i+40)*math.sin(x*(i+1)),(40*i+40)*math.cos(x*(i+1))
+            for i in range(6):
+                t[i].shape('circle')
+                t[i].color(colors[i])
+                t[i].goto((40*i+40)*math.sin(x*(i+1)),(40*i+40)*math.cos(x*(i+1))
 orbit()
